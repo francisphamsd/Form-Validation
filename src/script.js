7accf60640a71256bb44d56d730a93c28087fbce
francisphamsd <93688068+francisphamsd@users.noreply.github.com>
2021-11-04 22:54:47 -0700

    Style sheet for login page Finished with log in page // TODO: link to reset password page and sign up page

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const userid = document.getElementById("userid");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
